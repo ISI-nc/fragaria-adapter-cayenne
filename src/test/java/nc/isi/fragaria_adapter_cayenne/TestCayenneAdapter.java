5157613f614b2c22dc63444872f90fed0ce3944a
Brounchy <bjonathas@isi.nc>
2013-01-31 16:58:00 +1100

    début des tests

---
+++ src/test/java/nc/isi/fragaria_adapter_cayenne/TestCayenneAdapter.java
@@ -0,0 +1,7 @@
+package nc.isi.fragaria_adapter_cayenne;
+
+import junit.framework.TestCase;
+
+public class TestCayenneAdapter  extends TestCase {
+
+}
---
+++ src/test/java/nc/isi/fragaria_adapter_cayenne/views/TestCayenneViewConfigBuilder.java
@@ -0,0 +1,7 @@
+package nc.isi.fragaria_adapter_cayenne.views;
+
+import junit.framework.TestCase;
+
+public class TestCayenneViewConfigBuilder  extends TestCase{
+
+}
